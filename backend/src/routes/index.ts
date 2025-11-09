bc245599779ec27fdc1f1f2e5a26970de9f1ed25
IsaqueTADS <isaqueofc18@gmail.com>
2025-11-09 01:08:42 -0300

    rota para completar task

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,6 +3,7 @@ import { login } from './auth/login'
 import { register } from './auth/register'
 import { createTask } from './tasks/create-task'
 import { listTasks } from './tasks/list-task'
+import { completeTask } from './tasks/complete-task'
 
 export const registerRoutes: FastifyPluginAsyncZod = async (app) => {
   app.register(register, { prefix: '/auth' })
@@ -10,4 +11,5 @@ export const registerRoutes: FastifyPluginAsyncZod = async (app) => {
 
   app.register(createTask, { prefix: '/tasks' })
   app.register(listTasks, { prefix: '/tasks' })
+  app.register(completeTask, { prefix: '/tasks' })
 }
---
+++ backend/src/routes/tasks/complete-task.ts
@@ -0,0 +1,51 @@
+import { and, eq } from 'drizzle-orm'
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { db } from '@/db/client'
+import { tasks } from '@/db/schema'
+import { checkRequestJWT } from '@/middleware/check-request-jwt'
+import { getAuthenticatedUserFromRequest } from '@/utils/get-authenticated-user-from-request'
+
+export const completeTask: FastifyPluginAsyncZod = async (app) => {
+  app.patch(
+    '/:taskId/complete',
+    {
+      preHandler: [checkRequestJWT],
+      schema: {
+        tags: ['Tasks'],
+        summary: 'complete task',
+        params: z.object({
+          taskId: z.string(),
+        }),
+        response: {
+          404: z.object({
+            message: z.string().describe('Task não encontrada!'),
+          }),
+          200: z
+            .object({ message: z.string() })
+            .describe('Task concluída com sucesso'),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { taskId } = request.params
+      const userId = getAuthenticatedUserFromRequest(request).sub
+
+      const taskExist = await db
+        .select()
+        .from(tasks)
+        .where(and(eq(tasks.userId, userId), eq(tasks.id, taskId)))
+
+      if (taskExist.length === 0) {
+        return reply.status(404).send({ message: 'Task não encontrada!' })
+      }
+
+      await db
+        .update(tasks)
+        .set({ completed: true })
+        .where(and(eq(tasks.userId, userId), eq(tasks.id, taskId)))
+
+      reply.status(200).send({ message: 'Task concluída com sucesso' })
+    },
+  )
+}
