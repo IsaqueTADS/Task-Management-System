0f406040199745fa25f35cd63b3b7a65f90529dc
IsaqueTADS <isaqueofc18@gmail.com>
2025-11-20 18:56:38 -0300

    novas fontes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-primary)", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        blinker: ["Blinker", "sans-serif"],
+      },
+    },
+  },
+};
